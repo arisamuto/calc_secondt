3233a497202068cec6ba5018fb5b3f31074750c7
arisamuto <cookiemonster.ca17@gmail.com>
2019-06-19 13:19:37 +0900

    Initial Commit

---
+++ calc_secondt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calc_secondt
+//
+//  Created by Arisa Muto on 2019/06/19.
+//  Copyright © 2019 Arisa Muto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
